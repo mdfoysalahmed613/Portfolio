7baca3d2d161b89e30c07124d4475ca58baf9e37
Md Foysal Ahmed <mdfoysalahmed613@gmail.com>
2025-10-21 22:09:28 +0600

    fix nav-bar type error

--- app/(root)/skills/page.tsx
+++ app/(root)/skills/page.tsx
@@ -5,9 +5,10 @@ import React from 'react'
 const page = () => {
   return (
      <div id='skills'>
-        <h1 className='font-bold text-2xl md:text-4xl lg:text-5xl my-4'>Skills</h1>
+        <h1 className='font-bold text-4xl lg:text-5xl my-4'>Skills</h1>
       <p className='text-muted-foreground'>Key skills that define my professional identity.</p>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6'>
+      <hr className="my-6" />
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6 mx-6'>
         {skills.map((skill) => (
           <Skill key={skill.name} {...skill} />
         ))}
