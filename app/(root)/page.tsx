19a92f84084f06f1d04dfb189d3813c441c86200
Md Foysal Ahmed <mdfoysalahmed613@gmail.com>
2025-10-21 15:11:55 +0600

    add Footer

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,13 @@
+import Footer from '@/components/common/footer'
 import HeroSection from '@/components/home/hero-section'
-import Projects from '@/components/home/projects'
 import Skills from '@/components/home/skills'
 import React from 'react'
-
 const Page = () => {
   return (
     <div>
       <HeroSection />
       <Skills />
-      <Projects />
+      <Footer />
     </div>
   )
 }
---
+++ components/common/footer.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+import { FaLinkedin } from 'react-icons/fa6'
+import { SiGithub, SiGmail } from 'react-icons/si'
+
+const Footer = () => {
+  return (
+    <div className='flex justify-center gap-6 md:gap-12 py-12'>
+      <Link target='_blank' href='https://github.com/mdfoysalahmed613'><SiGithub size={24} /></Link>
+      <Link target='_blank' href='https://www.linkedin.com/in/mdfoysalahmed613'><FaLinkedin size={24} /></Link>
+      <Link target='_blank' href='mailto:mdfoysalahmed613@gmail.com'><SiGmail size={24} /></Link>
+    </div>
+  )
+}
+
+export default Footer
