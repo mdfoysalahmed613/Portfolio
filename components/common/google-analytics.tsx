e87839abdfcd44e4eb3dacfbc44ec8af3cc7a1d2
Md Foysal Ahmed <mdfoysalahmed613@gmail.com>
2025-11-19 19:56:04 +0600

    fix: update Google Analytics script loading strategy for improved performance

--- components/common/google-analytics.tsx
+++ components/common/google-analytics.tsx
@@ -7,9 +7,9 @@ export default function GoogleAnalytics() {
       <>
          <Script
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-            strategy="afterInteractive"
+            async
          />
-         <Script id="google-analytics" strategy="afterInteractive">
+         <Script>
             {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
@@ -17,6 +17,14 @@ export default function GoogleAnalytics() {
           gtag('config', '${GA_MEASUREMENT_ID}');
         `}
          </Script>
+         {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-J9NH53YYM5"></script>
+         <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-J9NH53YYM5');
+         </script> */}
       </>
    );
 }
