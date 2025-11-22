35f66552b675605c0e2105ab721e362a3b1a871d
Md Foysal Ahmed <mdfoysalahmed613@gmail.com>
2025-11-22 17:10:46 +0600

    feat: enhance hamburger menu with animation effects for improved user experience

--- components/common/hamburger.tsx
+++ components/common/hamburger.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { Menu, X } from 'lucide-react';
 import Link from 'next/link';
 import { navItems } from '@/lib/nav-items';
+import { motion } from 'framer-motion';
 
 const Hamburger = () => {
    const [showMenu, setShowMenu] = useState(false);
@@ -17,22 +18,34 @@ const Hamburger = () => {
             </div>
          </button>
          {showMenu && (
-            <div
-               className="absolute top-20 left-0 w-full bg-background/90 backdrop-blur-md flex flex-col items-center py-4 md:hidden z-20">
-               {navItems?.map((item, index) => (
-                  <div
-                     key={index}
-                     onClick={() => setShowMenu(false)}
-                  >
-                     <Link
-                        href={item.href}
-                        className="block w-full px-4 py-2 text-lg font-medium text-muted-foreground hover:text-muted-foreground/80 sm:text-sm">
-                        {item.title}
-                     </Link>
-                  </div>
+            <>
+               <div
+                  className="fixed inset-0 z-10 md:hidden"
+                  onClick={() => setShowMenu(false)}
+               />
+               <motion.div
+                  initial={{ opacity: 0, y: -20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.3, ease: "easeOut" }}
+                  className="absolute top-20 left-0 w-full bg-background/90 backdrop-blur-md flex flex-col items-center py-4 md:hidden z-20">
+                  {navItems?.map((item, index) => (
+                     <motion.div
+                        initial={{ opacity: 0, y: -10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.3, ease: "easeOut", delay: index * 0.1 }}
+                        key={index}
+                        onClick={() => setShowMenu(false)}
+                     >
+                        <Link
+                           href={item.href}
+                           className="block w-full px-4 py-2 text-lg font-medium text-muted-foreground hover:text-muted-foreground/80 sm:text-sm">
+                           {item.title}
+                        </Link>
+                     </motion.div>
 
-               ))}
-            </div>
+                  ))}
+               </motion.div>
+            </>
          )}
       </>
    )
