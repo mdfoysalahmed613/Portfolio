b7e63a66c3320fa5c76a9e3ffac062e224f42de4
Md Foysal Ahmed <mdfoysalahmed613@gmail.com>
2025-12-10 13:36:35 +0600

    fix: comment out Projects component in main page

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const Page = () => {
       <HeroSection />
       <Skills />
       <Services />
-      <Projects />
+      {/* <Projects /> */}
       <WorkingProcess />
       <Contact />
       <ScrollToTop />
