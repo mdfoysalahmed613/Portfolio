c956fcea9674675eb64eda52126e3d1437d68ef1
Md Foysal Ahmed <mdfoysalahmed613@gmail.com>
2025-12-23 23:49:52 +0600

    feat: update sitemap to remove dynamic blog pages and add next-themes dependency

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,4 @@
 import { MetadataRoute } from "next";
-import { blogs } from "@/lib/blogs";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://foysal.me";
@@ -28,13 +27,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
   //   priority: 0.8,
   // }));
 
-  // Dynamic blog pages
-  const blogPages = blogs.map((blog) => ({
-    url: `${baseUrl}/blog/${blog.slug}`,
-    lastModified: new Date(blog.publishDate),
-    changeFrequency: "weekly" as const,
-    priority: 0.7,
-  }));
 
-  return [...staticPages, ...blogPages];
+
+  // Dynamic blog pages
+
+  
+
+  return [...staticPages];
 }
